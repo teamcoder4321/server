60ca8cb1e0baf3fa8000dc268aaabc245b2f5417
teamcoder4321 <saurabhshah255@gmail.com>
2024-09-20 05:28:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get ('/login',(req, res) => {
+    res.send( '<h1>INVOICE SYSTEM</h1>' )
+})
+
+app.listen(process.env.port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
